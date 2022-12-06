003c2c8deea5d65d2bdea6afb8b5949a159120c9
franco <withamadness@gmail.com>
2022-12-06 11:39:52 -0500

    mostrando los datos

---
+++ android/app/src/main/kotlin/com/livcode/tareas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.livcode.tareas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  CollectionReference tasksReference = FirebaseFirestore.instance.collection('tasks');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Firebase")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ElevatedButton(onPressed: (){
+            tasksReference.get().then((value){
+              //QuerySnapshot collection = value;
+              //List<QueryDocumentSnapshot> docs = collection.docs;
+              //QueryDocumentSnapshot doc = docs[0];
+              //print(doc.data());
+              QuerySnapshot collection = value;
+              collection.docs.forEach((QueryDocumentSnapshot element) {
+                Map<String, dynamic> myMap = element.data() as Map<String,dynamic>;
+                print(myMap["title"]);
+
+               });
+            });
+          }, 
+          child: Text("Obtener la data"))
+
+        ],),
+      ),
+    );
+  }
+}
+
