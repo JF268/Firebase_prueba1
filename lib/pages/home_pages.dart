b720dbb9c7b29463ba1861ca2c7f24b5063dca39
franco <withamadness@gmail.com>
2022-12-07 10:30:41 -0500

    obtener informacion con streambuilder

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   CollectionReference tasksReference = FirebaseFirestore.instance.collection('tasks');
-
+/*
   Stream<int> counter() async*{
     for(int i=0;i<10;i++){
       yield i;
@@ -13,19 +13,51 @@ class HomePage extends StatelessWidget {
   Future<int> getNumber() async{
     return 1000;
   }
+  */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Firebase")),
-      body: StreamBuilder(stream: counter() ,
+      body:StreamBuilder(
+        stream: tasksReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if(snap.hasData){
+            QuerySnapshot collection = snap.data;
+            List<QueryDocumentSnapshot> docs = collection.docs;
+            List<Map<String, dynamic>> docsMap = docs.map((e)=> e.data() as Map<String,dynamic>).toList();
+            print(docsMap);
+            return ListView.builder(
+              itemCount: docsMap.length,
+              itemBuilder:(BuildContext context, int index){
+                return ListTile( 
+                  title: Text(docsMap[index]["title"]),
+                );
+              }
+              );
+          }
+          return Center(child: CircularProgressIndicator(),);
+        }
+      )
+      /*
+      StreamBuilder(stream: counter() ,
+      builder: (BuildContext context, AsyncSnapshot snap){
+        if(snap.hasData){
+          QuerySnapshot collection = snap.data;
+        }
+        return Center(child: CircularProgressIndicator(),);
+      },
+
+      /* llamando a la instrucción de stream
+      StreamBuilder(stream: counter() ,
       builder: (BuildContext context, AsyncSnapshot snapshot){
         if(snapshot.hasData){
           int data = snapshot.data;
           return Center(child: Text(data.toString(),),);
         }
         return Center(child: CircularProgressIndicator(),);
-      },
+      },*/
       )
+      */
     );
   }
 }
