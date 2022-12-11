62d3ab9b0c1211d7da2b614dae34575f714a2e2b
franco <withamadness@gmail.com>
2022-12-11 00:27:34 -0500

    login parte 2/opciones de inicio con Google y Facebook

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tareas/ui/general/colors.dart';
+import 'package:tareas/ui/widgets/button_custom_widget.dart';
 import 'package:tareas/ui/widgets/button_normal-widget.dart';
 import 'package:tareas/ui/widgets/general_widgets.dart';
 import 'package:tareas/ui/widgets/textfield_normal_widget.dart';
@@ -42,7 +43,21 @@ class _LoginPageState extends State<LoginPage> {
               divider20(),
               TextFieldPasswordWidget(controller: _passwordController),
               divider10(),
-              ButtonNormalWidget(onPressed: (){}),
+              ButtonCustomWidget(text: "Hola", 
+              color: kBrandPrymaryColor, 
+              icon: "check"),
+              divider10(),
+              Text("O ingresa con tus redes sociales"),
+              divider20(),
+              ButtonCustomWidget(
+                text: "Iniciar sesión con Google", 
+                color: Color(0xfff84b2a), 
+                icon: "google"),
+              divider20(),
+              ButtonCustomWidget(
+                text: "Iniciar sesión con Facebook", 
+                color: Color(0xfff507CC0), 
+                icon: "facebook"),
             ],
           ),
           padding: EdgeInsets.all(16.0)),
---
+++ lib/ui/widgets/button_custom_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tareas/ui/general/colors.dart';
+
+class ButtonCustomWidget extends StatelessWidget {
+  String text;
+  Color color;
+  String icon;
+
+  ButtonCustomWidget({
+    required this.text,
+    required this.color,
+    required this.icon
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 40.0,
+        width: double.infinity,
+        child: ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: color,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(14),
+              ),
+            ),
+            icon: SvgPicture.asset("assets/icons/$icon.svg",color: Colors.white,),
+            label: Text(
+              text,
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w600),
+            )));
+  }
+}
